c68f6b84d813733acca2475dcfa4b0075161801e
Joakim Bech <joakim.bech@linaro.org>
2019-11-27 09:18:19 +0100

    Use group id from host user
    To be able to make it easier to share ccache, let's set the group to the
    same as the user in the host.
    
    If all files in ccache have:
     find $CCACHE_DIR -type d | xargs chmod g+ws
    
    Then it should be fine to share the ccache.
    
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-docker run -ti \
+IMAGE=$1
+
+export GID=$(id -g)
+
+docker run -it \
 	-e DISPLAY=$DISPLAY \
+	--user optee:$GID \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /home/jbech/devel/optee_projects/reference:/home/optee/reference \
 	-v /home/jbech/.ccache:/home/optee/.ccache \
-	optee_mnt
+	$1
